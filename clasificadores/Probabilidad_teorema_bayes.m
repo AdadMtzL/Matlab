cb07e979b64483a62f81e12c37b8f76855452767
Adad Marcel <adadmarcelmartinez@gmail.com>
2022-09-04 21:58:24 -0500

    Cambios varios
    se añadieron clases, comentarios con el proposito de comprender mejor el funcionamiento

--- clasificadores/Probabilidad_teorema_bayes.m
+++ clasificadores/Probabilidad_teorema_bayes.m
@@ -3,7 +3,7 @@ close all
 clear all 
 warning off all 
 disp('Welcome to pattern recognition')
-%
+%   Esto determina un porcentaje de similitud ante otras clases
 %
 %               e^{-1(Distancia mahalobi)/2}
 %p(vec/clase)=------------------------------------------------------------
@@ -19,6 +19,29 @@ disp('Welcome to pattern recognition')
 
 %notas importantes : ^T indica la matriz transpuesta
 
+%declaración de clases y el vector desconocido
 c1 = [1 2 3 2 3; 2 2 4 7 9];
 c2 = [3 4 7 8 9; 5 6 -1 2 3];
 c3 = [10 10 11 12 13; 1 7 4 -2 9];
+vector=[7;5];
+
+%calculo de medias
+media1=mean(c1,2);
+media2=mean(c2,2);
+media3=mean(c3,2);
+
+%calculo de las matrices de varianza_covarianza
+matriz_var_cov_clase_1=(c1-media1);
+matriz_var_cov_clase_2=(c2-media2);
+matriz_var_cov_clase_3=(c3-media3);
+
+%calculo de la mat1
+mat1_1=vector-media1;
+mat1_2=vector-media2;
+mat1_3=vector-media3;
+
+%calculo de la mat2
+mat2_1=transpose(mat1_1);
+mat2_2=transpose(mat2_1);
+mat2_3=transpose(mat3_1);
+
