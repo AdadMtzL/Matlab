260a26d7aaa9b3f8da83e5dba0d0dd02b2d7b743
Adad Marcel <adadmarcelmartinez@gmail.com>
2022-09-04 21:46:48 -0500

    Más comentarios
    Esos comentarios son para explicar las formulas mejor

--- clasificadores/Probabilidad_teorema_bayes.m
+++ clasificadores/Probabilidad_teorema_bayes.m
@@ -9,5 +9,16 @@ disp('Welcome to pattern recognition')
 %p(vec/clase)=------------------------------------------------------------
 %            (2pi)^{n/2}*[determinante(matriz_varianza_covarianza)]^{1/2}
 %
-%
-c1=[1 2;3 4];
+%distancia de mahalobis = (vector - media_clase)^T *Matriz_inversa_covarianza_clase(x-media_clase)*(vector - media_clase)
+%                                  ^                               ^                                 ^
+%                                  |                               |                                 |
+%                                 mat2                           mat_inv_cov                        mat1
+
+
+%                          matriz_covarianza_clase=(clase-media_clase)*(clase-media_clase)^T
+
+%notas importantes : ^T indica la matriz transpuesta
+
+c1 = [1 2 3 2 3; 2 2 4 7 9];
+c2 = [3 4 7 8 9; 5 6 -1 2 3];
+c3 = [10 10 11 12 13; 1 7 4 -2 9];
