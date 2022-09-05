8eae25da4217d8bad3e6d229f5841e4337a9bc9c
Adad Marcel <adadmarcelmartinez@gmail.com>
2022-09-04 21:39:23 -0500

    Comentarios
    Adicion de comentarios para explicar el funcionamiento

---
+++ clasificadores/Probabilidad_teorema_bayes.m
@@ -0,0 +1,13 @@
+clc 
+close all 
+clear all 
+warning off all 
+disp('Welcome to pattern recognition')
+%
+%
+%               e^{-1(Distancia mahalobi)/2}
+%p(vec/clase)=------------------------------------------------------------
+%            (2pi)^{n/2}*[determinante(matriz_varianza_covarianza)]^{1/2}
+%
+%
+c1=[1 2;3 4];
