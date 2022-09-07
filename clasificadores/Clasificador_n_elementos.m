190b10582b58061bb660cfa4b6511f9462cc14a5
Adad Marcel <adadmarcelmartinez@gmail.com>
2022-09-07 12:02:03 -0500

    Programa base para la práctica 3
    Muestra como generar n elementos por clase, además de como determinar su centroide (x,y)

---
+++ clasificadores/Clasificador_n_elementos.m
@@ -0,0 +1,31 @@
+clc 
+clear all
+close all
+warning off all
+
+%Este programa genera clases con n elementos aleatorios
+
+%generación de n elementos por clase
+n=100000;
+c1x=randn(1,n)+3;
+c1y=randn(1,n)+3;
+
+c2x=randn(1,n)-2; %+-n indica el centroide de los puntos aleatorios
+c2y=randn(1,n)-2;
+
+c3=[randn(1,n)+8;randn(1,n)+8];
+plot(c1x(1,:),c1y(1,:),'ro','MarkerFaceColor','r','MarkerSize',5);
+hold on
+grid on
+plot(c2x(1,:),c2y(1,:),'g*','MarkerFaceColor','g','MarkerSize',5);
+plot(c3(1,:),c3(2,:),'b+','MarkerFaceColor','b','MarkerSize',5);
+
+legend('clase1','clase2','clase3');
+
+%Practica 3: Pedir al usuario generar n clases con n representantes por clase %
+%pedir la ubicación del vector desconocido.
+%Ubicación (x,y) del centroide de cada clase%
+%Dispersion (x,y) por cada clase%
+% Menu interactivo que practica quere selecciona para determinar la distancia.
+%
+%
