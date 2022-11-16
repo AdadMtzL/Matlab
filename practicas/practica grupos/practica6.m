429445f2e839c09a76c0c68dd3c15986dc4c9205
JohanCampos <43768539+JohanCampos@users.noreply.github.com>
2022-11-16 15:18:37 -0600

    PrevioPaso realizado

--- practicas/practica grupos/practica6.m
+++ practicas/practica grupos/practica6.m
@@ -20,9 +20,8 @@ n = 0;
 x = 0;
 y = 0;
 puntos = [];
-minimo = 1;
 
-grupos = [0];
+grupos = [];
 %Paso previo
 n = input("Digite la cantidad de puntos\n");
 cantidadPuntos = n;
@@ -30,19 +29,19 @@ for i = 1 : n
     x = input("Valor en x del punto "+i+"\n");
     y = input("Valor en y del punto "+i+"\n");
     %composición de puntos: [coordenada x, coordenada y, identificador]
-    puntos(i) = [x,y,i]; 
+    puntos{i} = [x,y,i]; 
 end
 
 %Paso previo
 nuevoGrupo = 1;
 for i = 1 : n
-    puntoColumna = puntos(i);
+    puntoColumna = puntos{i};
     grupo = [];
     contador = 1;
     for j = 1 : n
         %Nota
         if i ~= j
-            puntoFila = puntos(j);
+            puntoFila = puntos{j};
             distanciaX = puntoColumna(1)-puntoFila(1);
             distanciaY = puntoColumna(2)-puntoFila(2);
             if distanciaX < 0
@@ -52,14 +51,24 @@ for i = 1 : n
                 distanciaY = distanciaY * -1;
             end
             distancia = distanciaX + distanciaY;
-            grupo(contador) = [puntoFila(3),distancia];
+            grupo{contador} = [puntoFila(3),distancia];
             contador = contador + 1;
         end
     end
     %Funcion para organizar los puntos
-
+    [grupo] = ordenarMenorMayor(grupo,contador-1);
     %Agregamos el grupo a nuestro array de grupos
-    
+    grupos{nuevoGrupo} = grupo;
+    nuevoGrupo = nuevoGrupo + 1;
+end
+%Paso 5
+
+%Paso 1
+minimo = grupos{1}{1};
+for i=2 : nuevoGrupo-1
+    if  grupos{i}{1}(2) < minimo(2)
+        minimo = grupos{i}{1};
+    end
 end
 
 %Funcion para organizar los puntos
@@ -70,42 +79,38 @@ function [grupoOrdenado] = ordenarMenorMayor(grupo,total)
     %tomamos los primeros dos puntos y hacemos una comparativa de cual es
     %el de menor distancia y organizamos de derecha a izquierda, derecha
     %siendo el menor
-    puntoUno = grupo(1);
-    puntoDos = grupo(2);
+    puntoUno = grupo{1};
+    puntoDos = grupo{2};
     if puntoUno(2)<puntoDos(2)
-        grupoOrdenado(aumento) = puntoUno;
+        grupoOrdenado{aumento} = puntoUno;
         aumento = aumento + 1;
-        grupoOrdenado(aumento) = puntoDos;
-        aumento = aumento + 1;
+        grupoOrdenado{aumento} = puntoDos;
     elseif puntoUno(2)>puntoDos(2)
-        grupoOrdenado(aumento) = puntoDos;
+        grupoOrdenado{aumento} = puntoDos;
         aumento = aumento + 1;
-        grupoOrdenado(aumento) = puntoUno;
-        aumento = aumento + 1;
+        grupoOrdenado{aumento} = puntoUno;
     elseif puntoUno(2)==puntoDos(2)
         if puntoUno(1)<puntoDos(1)
-            grupoOrdenado(aumento) = puntoUno;
+            grupoOrdenado{aumento} = puntoUno;
             aumento = aumento + 1;
-            grupoOrdenado(aumento) = puntoDos;
-            aumento = aumento + 1;
+            grupoOrdenado{aumento} = puntoDos;
         elseif puntoUno(1)>puntoDos(1)
-            grupoOrdenado(aumento) = puntoDos;
+            grupoOrdenado{aumento} = puntoDos;
             aumento = aumento + 1;
-            grupoOrdenado(aumento) = puntoUno;
-            aumento = aumento + 1;
+            grupoOrdenado{aumento} = puntoUno;
         end
     end
     %para los puntos restantes
     for i=3 : total
         %tomamos el punto siguiente
-        punto = grupo(i);
+        punto = grupo{i};
         %recorremos nuestros puntos guardados y comparamos hasta que
         %encontremos que sea menor o igual a un punto
         posicion=0;
         for j=1 : aumento
-            puntoOrdenado(i)
-            if punto(2)<=puntoOrdenado
-                posicion = i;
+            puntoOrdenado = grupoOrdenado{j};
+            if punto(2)<=puntoOrdenado(2)
+                posicion = j;
                 break;
             end
         end
@@ -115,17 +120,18 @@ function [grupoOrdenado] = ordenarMenorMayor(grupo,total)
             %ordenado
             auxOrdenado = grupoOrdenado;
             %guardamos el punto en la posicion encontrada
-            grupoOrdenado(posicion) = punto; 
+            grupoOrdenado{posicion} = punto; 
             %agregamos los demas puntos que teniamos despues de este punto
             %nuevo
-            for i=posicion : aumento
-                grupoOrdenado(i+1) = auxOrdenado(i);
-            end
+            posicion = posicion + 1;
             aumento = aumento + 1;
+            for k=posicion : aumento
+                grupoOrdenado{k} = auxOrdenado{k-1};
+            end
         elseif posicion == 0
             %Si no fue menor a ninguno ya guardado, se guarda al final
-            grupoOrdenado(aumento) = punto;
             aumento = aumento + 1;
+            grupoOrdenado{aumento} = punto;
         end
     end
 end
