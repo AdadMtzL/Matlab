0514e43963e0eff7f8f98ddfba9e20bb20c373f1
JohanCampos <43768539+JohanCampos@users.noreply.github.com>
2022-11-15 21:00:16 -0600

    actualizacion de practica grupos

--- practicas/practica grupos/practica6.m
+++ practicas/practica grupos/practica6.m
@@ -20,21 +20,69 @@ warning off all %evita llamadas de atención
 n = 0;
 x = 0;
 y = 0;
-puntos = [];
+puntos = [0];
+minimo = 1;
 
-grupos = [];
+grupos = [0];
 %Paso previo
 n = input("Digite la cantidad de puntos");
 cantidadPuntos = n;
 for i = 1 : n
     x = input("Valor en x del punto "+i);
     y = input("Valor en y del punto "+i);
-    puntos(i) = [x,y]; 
+    puntos(i) = [x,y,i]; 
 end
 
 %Paso 1: no necesario hacer
 %Paso 5
+contador = 1;
+puntosAux = puntos;
 while cantidadPuntos ~= 1
-    %Paso 2
+    minimo = 1;
+    puntoMinimo1 = 0;
+    puntoMinimo2 = 0;
+    %Paso 2: no es necesario hacer, solo requerimos la operación y el valor
+    %resultante
+    %Paso 3
+    for i = 1 : cantidadPuntos
+        puntoColumna = puntosAux(i);
+        for j = 1 : cantidadPuntos
+            puntoFila = puntosAux(j);
+            distanciaX = puntoColumna(1)-puntoFila(1);
+            distanciaY = puntoColumna(2)-puntoFila(2);
+            if distanciaX < 0
+                distanciaX = distanciaX * -1;
+            end
+            if distanciaY < 0
+                distanciaY = distanciaY * -1;
+            end
+            distancia = distanciaX + distanciaY;
+            if i == 1 && j == 2
+                minimo = distancia;
+                puntoMinimo1 = puntoColumna(3);
+                puntoMinimo2 = puntoFila(3);
+            else
+                if distancia == 0
+                    continue;
+                elseif distancia <= minimo
+                    minimo = distancia;
+                    puntoMinimo1 = puntoColumna(3);
+                    puntoMinimo2 = puntoFila(3);
+                end
+            end
+        end
+    end
+    grupos(contador) = [puntoMinimo1,puntoMinimo2,distancia];
+    contador = contador + 1;
+    puntosAux = restablecerPuntos(puntosAux, puntoMinimo1, puntoMinimo2, cantidadPuntos);
+end
 
+function [puntosNuevos] = restablecerPuntos(puntos, punto1, punto2, n)
+    for i = 1 : n
+        if puntos(i) == punto1
+            
+        elseif puntos(i) == punto2
+        
+        end
+    end
 end
