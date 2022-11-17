36d9de0007b9771446514e66539cd0ff39229c9f
JohanCampos <43768539+JohanCampos@users.noreply.github.com>
2022-11-17 11:10:29 -0600

    Practica7 posicion jerarquica completada

--- practicas/practica grupos/practica6.m
+++ practicas/practica grupos/practica6.m
@@ -13,7 +13,21 @@ warning off all %evita llamadas de atención
 %Nota paso previo: No nos interesa la distancia del punto entre si mismo.
 %Paso 1: tomar el primer valor de cada tabla y obtener el primer grupo con
 %la menor distancia entre ellos.
-
+%Paso 2: Hacer nuevo grupo de nombre total+1 que una ambos grupos con la
+%distancia minima encontrada. 
+%Nota: para guardar las posiciones, los grupos que esten hasta el maximo de
+%n se agregan directamente, los que sean mas de n, se debe buscar entre los
+%grupos guardados hasta que tenga 2 grupos menores a n, de ahi guardamos
+%en la posicion el promedio de ambos grupos
+%   Paso 2.1: Para unir los puntos de ambos grupos, buscamos del primer grupo,
+%   el punto en el segundo grupo, si no hay, se agrega directamente, si hay,
+%   se toma el de menor distancia, así hasta verificar que en el segundo grupo no
+%   haya puntos.
+%   Nota: tomamos desde el segundo punto de cada grupo, ya que, el primero
+%   fue el que hizo el grupo ya descartado
+%Paso 3: Eliminamos los grupos unidos y agregamos el nuevo grupo al final
+%de nuestros grupos
+%   Paso 3.1: Se debe cambiar los puntos encontrados en cada grupo por 
 
 %variables
 n = 0;
@@ -22,9 +36,12 @@ y = 0;
 puntos = [];
 
 grupos = [];
+ban = [0];
+aumentoBan = 1;
+gruposAMostrar = [];
+contadorGruposAMostrar = 1;
 %Paso previo
 n = input("Digite la cantidad de puntos\n");
-cantidadPuntos = n;
 for i = 1 : n
     x = input("Valor en x del punto "+i+"\n");
     y = input("Valor en y del punto "+i+"\n");
@@ -51,6 +68,7 @@ for i = 1 : n
                 distanciaY = distanciaY * -1;
             end
             distancia = distanciaX + distanciaY;
+            %composicion de grupo: [punto destino, distancia entre el punto del grupo y el destino]
             grupo{contador} = [puntoFila(3),distancia];
             contador = contador + 1;
         end
@@ -61,13 +79,113 @@ for i = 1 : n
     grupos{nuevoGrupo} = grupo;
     nuevoGrupo = nuevoGrupo + 1;
 end
-%Paso 5
 
-%Paso 1
-minimo = grupos{1}{1};
-for i=2 : nuevoGrupo-1
-    if  grupos{i}{1}(2) < minimo(2)
-        minimo = grupos{i}{1};
+%Paso 4
+while (nuevoGrupo-aumentoBan) ~= 1
+    grupoNoBanneado = 1;
+    for i = 1 : aumentoBan-1
+        if ban(i) == grupoNoBanneado
+            grupoNoBanneado = grupoNoBanneado + 1;
+        end
+    end
+    %Paso 1
+    minimo = grupos{grupoNoBanneado}{1};
+    primerGrupo = grupoNoBanneado;
+    for i=1 : nuevoGrupo-1
+        validarBanneado = false;
+        for j=1 : aumentoBan-1
+            if ban(j) == i
+                validarBanneado = true;
+                break;
+            end
+        end
+        if validarBanneado == false
+            if  grupos{i}{1}(2) < minimo(2)
+                minimo = grupos{i}{1};
+                primerGrupo = i;
+            end
+        end
+    end
+    
+    tamanoPrimerGrupo = size(grupos{primerGrupo});
+    tamanoSegundoGrupo = size(grupos{minimo(1)});
+    %Agregamos al arreglo final el grupo
+    %Paso 2
+    %composicion de gruposAMostrar: [punto inicio, punto final, distancia]
+    %Nota
+    if primerGrupo <= n && minimo(1) <= n
+        gruposAMostrar{contadorGruposAMostrar}=[primerGrupo,minimo(1),minimo(2)];
+        contadorGruposAMostrar = contadorGruposAMostrar + 1;
+    else
+        valorUno = primerGrupo;
+        valorDos = minimo(1);
+        if  valorUno > n
+            grupo = gruposAMostrar{valorUno-n};
+            valorUno = (grupo(1)+grupo(2))/2;
+        end
+        if  valorDos > n
+            grupo = gruposAMostrar{valorDos-n};
+            valorDos = (grupo(1)+grupo(2))/2;
+        end
+        gruposAMostrar{contadorGruposAMostrar}=[valorUno,valorDos,minimo(2)];
+        contadorGruposAMostrar = contadorGruposAMostrar + 1;
+    end
+    
+    %Paso 2.1
+    grupoNuevo = [];
+    aumentoNuevo = 1;
+    for i = 2 : tamanoPrimerGrupo(2)
+        punto = grupos{primerGrupo}{i};
+        validarPunto = false;
+        for j = 2 : tamanoSegundoGrupo(2)
+            puntoComparar = grupos{minimo(1)}{j};
+            if punto(1) == puntoComparar(1)
+                validarPunto = true;
+                %Buscamos el de menor distancia
+                if punto(2) <= puntoComparar(2)
+                    grupoNuevo{aumentoNuevo} = punto;
+                    aumentoNuevo = aumentoNuevo + 1;
+                else
+                    grupoNuevo{aumentoNuevo} = puntoComparar;
+                    aumentoNuevo = aumentoNuevo + 1;
+                end
+                break;
+            end
+        end
+        if validarPunto == false
+            grupoNuevo{aumentoNuevo} = punto;
+            aumentoNuevo = aumentoNuevo + 1;
+        end
+    end
+    %Paso 3:
+    ban(aumentoBan) = primerGrupo;
+    aumentoBan = aumentoBan + 1;
+    ban(aumentoBan) = minimo(1);
+    aumentoBan = aumentoBan + 1;
+    ban = sort(ban);
+    %[grupoNuevo] = ordenarMenorMayor (grupoNuevo,aumentoNuevo-1);
+    [grupos] = actualizarPuntosGrupo(grupos,nuevoGrupo-1,primerGrupo,minimo(1));
+    grupos{nuevoGrupo} = grupoNuevo;
+    nuevoGrupo = nuevoGrupo + 1;
+end
+
+
+%Funcion para actualizar los puntos unidos por el nuevo grupo
+function [grupoActualizado] = actualizarPuntosGrupo(grupos, total, puntoUnoCambio, puntoDosCambio)
+    grupoActualizado = [];
+    grupo = [];
+    grupoNuevo = [];
+    for i = 1 : total
+        grupo = grupos{i};
+        tamano = size(grupo);
+        for j = 1 : tamano(2)
+            punto = grupo{j};
+            if punto(1) == puntoUnoCambio || punto(1) == puntoDosCambio
+                punto(1) = total+1; 
+            end
+            grupoNuevo{j} = punto;
+        end
+        grupoActualizado{i} = grupoNuevo;
     end
 end
 
