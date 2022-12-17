7a5944906a50aaa66736792f53a3ae6b10e70714
JohanCampos <43768539+JohanCampos@users.noreply.github.com>
2022-12-17 09:55:31 -0600

    Actualizo practica 7

--- practicas/practica grupos_7/practica7.m
+++ practicas/practica grupos_7/practica7.m
@@ -3,14 +3,9 @@ clear all %limpia todas las variables
 close all %cierra todo
 warning off all %evita llamadas de atención
 %Hacer grupos relacionando distancias
-%Notas: Al ocupar siempre los mismos puntos, las distancias nunca varian,
-%por lo que no hay que solo hay que calcular distancias una vez y de ahí
-%con los grupos, hacer comparativas y respetar jerarquias.
-%Nuevos pasos
 
 %Paso previo: calcular todas las distancias entre los puntos y hacer una tabla
 %con distancias de menor a mayor desde cada punto.
-%Nota paso previo: No nos interesa la distancia del punto entre si mismo.
 %Paso 1: tomar el primer valor de cada tabla y obtener el primer grupo con
 %la menor distancia entre ellos.
 %Paso 2: Hacer nuevo grupo de nombre total+1 que una ambos grupos con la
@@ -115,24 +110,8 @@ while (nuevoGrupo-aumentoBan) ~= 1
     %Paso 2
     %composicion de gruposAMostrar: [punto inicio, punto final, distancia]
     %Nota
-    %if primerGrupo <= n && minimo(1) <= n
-        gruposAMostrar{contadorGruposAMostrar}=[primerGrupo,minimo(1),minimo(2)];
-        contadorGruposAMostrar = contadorGruposAMostrar + 1;
-    %else
-    %    valorUno = primerGrupo;
-    %    valorDos = minimo(1);
-    %    if  valorUno > n
-    %        grupo = gruposAMostrar{valorUno-n};
-    %        valorUno = (grupo(1)+grupo(2))/2;
-    %    end
-    %    if  valorDos > n
-    %        grupo = gruposAMostrar{valorDos-n};
-    %        valorDos = (grupo(1)+grupo(2))/2;
-    %    end
-    %    gruposAMostrar{contadorGruposAMostrar}=[valorUno,valorDos,minimo(2)];
-    %    contadorGruposAMostrar = contadorGruposAMostrar + 1;
-    %end
-    
+    gruposAMostrar{contadorGruposAMostrar}=[primerGrupo,minimo(1),minimo(2)];
+    contadorGruposAMostrar = contadorGruposAMostrar + 1;
     %Paso 2.1
     grupoNuevo = [];
     aumentoNuevo = 1;
@@ -179,7 +158,6 @@ Final = reshape(G,[3 j(2)]);
 Final = transpose(Final);
 Final = ordenarSalida(Final,j(2));
 
-X = linkage(Final,"average");
 figure()
 dendrogram(Final)
 
@@ -279,15 +257,3 @@ function[arregloSort] = ordenarSalida(arreglo,dim)
         end
     end
 end
-
-%Pasos vistos en clase
-%Paso previo: pedir n, n puntos xy
-%Primer paso: "Ubicar los puntos en una grafica cuadriculada"
-%Segundo paso: "Hacer una tabla donde las filas y columnas sean los numeros
-%de los puntos"
-%Tercer paso: "Llenar la tabla calculando la distancia más corta entre los
-%puntos de cada celda"
-%Cuarto paso: "Buscar el primer grupo que tenga menor distancia entre algun
-%punto, juntar y hacer nuevo punto de nombre n+1"
-%Quinto paso: repetir los pasos del 2 al 4 quitando los puntos en grupo y
-%agregando el nuevo punto
