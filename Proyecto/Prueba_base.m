037e17800a07c650f1d839c656ea77732c6ad09d
Adad Marcel <adadmarcelmartinez@gmail.com>
2023-01-15 16:34:10 -0600

    Mejoras en la detección de objetos
    Se añadio el nombre de los objetos

--- Proyecto/Prueba_base.m
+++ Proyecto/Prueba_base.m
@@ -17,10 +17,10 @@ if (usuario == 1)
         end
 elseif (usuario == 2)
         %nombre = "C:\Users\19286463\Desktop\Escom\semestre actual\pattern\github\Matlab\Proyecto\Bases Sossa\IMAG";
-        nombre = "C:\Users\19286463\Desktop\Escom\semestre actual\pattern\proyecto\base_2\bmp\IMG";
+        nombre = "C:\Users\19286463\Desktop\Escom\semestre actual\pattern\proyecto\bmp_correcta\IMG";
         nombreInicialArchivo = "IMG";
         extension = ".bmp";
-        totalImagenes = 20;
+        totalImagenes = 10;
         nombres = gtr(nombreInicialArchivo,extension,totalImagenes,nombre);
         for imag = 1:1:length(nombres)
             ver_imagenes(nombres(imag));
@@ -37,6 +37,16 @@ elseif (usuario == 3)
             pausa = input("Cotinuar?  ");
         end
 end
+%funcones utiles para el futuro:
+% imagen_jpg = imread("C:\Users\19286463\Desktop\Escom\semestre actual\pattern\proyecto\jpg\IMG001.jpg");
+%imwrite(imagen_jpg,"C:\Users\19286463\Desktop\Escom\semestre actual\pattern\proyecto\bmp\IMG001.bmp");
+%imwrite(imagen_jpg,"C:\Users\19286463\Desktop\Escom\semestre actual\pattern\proyecto\bmp\IMG999.bmp");
+%F = imread("C:\Users\19286463\Desktop\Escom\semestre actual\pattern\proyecto\bmp\IMG999.bmp")
+%[IND,map] = rgb2ind(imagen_jpg,64);
+%imagesc(IND)
+%colormap(map)
+
+
 
 function get_rts = gtr(nombreInicialArchivo,extension,totalImagenes,nombre)
     numero = "";
@@ -70,7 +80,7 @@ function imgs = ver_imagenes(ruta_imagen)
     for i =1:1:length(stats)
             ttmmp = stats_1(i).BoundingBox;
             rectangle("Position",[ttmmp(1),ttmmp(2),ttmmp(3),ttmmp(4)],"EdgeColor","r","LineWidth",2);
-            text(stats(i).Centroid(1),stats(i).Centroid(2),num2str(i),"Color","g");
+            text(stats(i).Centroid(1),stats(i).Centroid(2),strcat("Objeto ",num2str(i)),"Color","g");
     end
     datos = split(ruta_imagen,'\');
     nombre = split(datos(length(datos)),'.');
